4b7378ad62ec95a06352ea65bd98ea3c7288f13b
begussimo <bgm.koca@gmail.com>
2024-03-07 16:48:05 +0100

    adding updates

---
+++ target/run/jaffle_shop/models/marts/core/fct_orders.sql
@@ -0,0 +1,39 @@
+
+  
+    
+
+        create or replace transient table ANALYTICS.PUBLIC.fct_orders
+         as
+        (with orders as  (
+    select * from ANALYTICS.PUBLIC.stg_orders
+),
+
+payments as (
+    select * from ANALYTICS.PUBLIC.stg_payments
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
+)
+
+select * from final
+        );
+      
+  
